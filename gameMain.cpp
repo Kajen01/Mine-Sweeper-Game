064d1fca0d4e8c3c178d59d5fc7d8aee161490fb
Kajen01 <prof.kajen01@gmail.com>
2024-01-10 12:40:05 +0530

    13

--- gameMain.cpp
+++ gameMain.cpp
@@ -145,4 +145,82 @@ int Minesweeper::convertLetterToIndex(char letter) {
     return toupper(letter) - 'A';
 }
 
+void Minesweeper::playGame() {
+    placeMines();
+
+    while (true) {
+        displayField();
+
+        cout << "Enter move (e.g., ABF for flag, ABR for reveal): ";
+        string move;
+        cin >> move;
+
+        if (move.size() != 3) {
+            cout << "Invalid move. Please enter a valid move." << endl;
+            continue;
+        }
+
+        int row = convertLetterToIndex(move[0]);
+        int col = convertLetterToIndex(move[1]);
+
+        if (row < 0 || row >= gridSize || col < 0 || col >= gridSize) {
+            cout << "Invalid move. Please enter a valid move." << endl;
+            continue;
+        }
+
+        char action = move[2];
+
+        if (action == 'F') {
+            placeFlag(row, col);
+        } else if (action == 'R') {
+            revealLocation(row, col);
+        } else {
+            cout << "Invalid move. Please enter a valid move." << endl;
+            continue;
+        }
+
+        if (checkWin()) {
+            cout << "Congratulations! You won!" << endl;
+            break;
+        }
+    }
+}
+
+int main()
+{  
+    cout << "1. 10-by-10 (12 Mines)" << endl;
+    cout << "2. 15-by-15 (18 Mines)" << endl;
+    cout << "3. 20-by-20 (24 Mines)" << endl;
+    cout << "\nSelect Field Option: ";
+
+    int option;
+    cin >> option;
+    
+    cout << endl << endl;
+
+    int gridSize, numMines;
+
+    switch (option) {
+    case 1:
+        gridSize = 10;
+        numMines = 12;
+        break;
+    case 2:
+        gridSize = 15;
+        numMines = 18;
+        break;
+    case 3:
+        gridSize = 20;
+        numMines = 24;
+        break;
+    default:
+        cout << "Invalid option. Exiting." << endl;
+        return 1;
+    }
+
+    Minesweeper game(gridSize, numMines);
+    game.playGame();
+
+    return 0;
+}
 
